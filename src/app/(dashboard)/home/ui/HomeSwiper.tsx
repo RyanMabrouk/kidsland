5af1d71a81bc7ef87dc09e830993e326282e35cd
MontassarSN <snenmontassar@gmail.com>
2024-09-23 11:15:39 +0100

    fixed some design issues for responsive

--- src/app/(dashboard)/home/ui/HomeSwiper.tsx
+++ src/app/(dashboard)/home/ui/HomeSwiper.tsx
@@ -5,28 +5,57 @@ import Image from "next/image";
 export default function HomeSwiper() {
   return (
     <div className="[&_.swiper-pagination-bullet-active]:bg-color1">
-      <CustomSwiper
-        slides={Array.from({ length: 4 }).map((_, i) => (
-          <Image
-            key={i}
-            src={`/home/banners/banner-${i + 1}.webp`}
-            alt=""
-            width={1920}
-            height={1000}
-            className="h-[50vh] w-full object-cover"
-          />
-        ))}
-        initialSlide={0}
-        slidesPerView={1}
-        pagination
-        loop
-        allowTouchMove
-        speed={1500}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: true,
-        }}
-      />
+      {/* PC Swiper for larger screens */}
+      <div className="hidden max-[600px]:hidden md:block">
+        <CustomSwiper
+          slides={Array.from({ length: 4 }).map((_, i) => (
+            <Image
+              key={i}
+              src={`/home/banners/bannerPc${i + 1}.jpg`}
+              alt=""
+              width={1920}
+              height={1000}
+              className="h-[50vh] w-full"
+            />
+          ))}
+          initialSlide={0}
+          slidesPerView={1}
+          pagination
+          loop
+          allowTouchMove
+          speed={1500}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: true,
+          }}
+        />
+      </div>
+
+      {/* Phone Swiper for screens <= 1150px */}
+      <div className="block max-[600px]:block md:hidden">
+        <CustomSwiper
+          slides={Array.from({ length: 4 }).map((_, i) => (
+            <Image
+              key={i}
+              src={`/home/banners/bannerPhone${i + 1}.jpg`}
+              alt=""
+              width={1920}
+              height={1000}
+              className="h-[50vh] w-full"
+            />
+          ))}
+          initialSlide={0}
+          slidesPerView={1}
+          pagination
+          loop
+          allowTouchMove
+          speed={1500}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: true,
+          }}
+        />
+      </div>
     </div>
   );
 }
