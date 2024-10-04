ff8262fc5b4440663757ed5a6662f814b7475ede
Rayen Mabrouk <mabroukr999@gmail.com>
2024-10-04 22:59:28 +0100

    Refactor OrderButton component to fix translation issue and improve code readability

--- src/app/(dashboard)/Cart/ui/Resume.tsx
+++ src/app/(dashboard)/Cart/ui/Resume.tsx
@@ -6,7 +6,7 @@ import useCart from "@/hooks/data/cart/useCart";
 export default function Resume() {
   const { data: cart } = useCart();
   const { data: translation } = useTranslation();
-  if (!cart ) {
+  if (!cart) {
     return (
       <div className="flex h-fit w-[21rem] flex-col items-center gap-2 rounded-xl bg-white p-4 shadow-2xl transition-all duration-300 max-md:w-full md:sticky md:top-20">
         <h1 className="text-2xl text-color1">
@@ -27,11 +27,11 @@ export default function Resume() {
         <h1 className="min-w-max text-2xl font-semibold">
           {translation?.lang["Total"]}:
         </h1>
-        <div className="overflow-hidden whitespace-nowrap text-xl">
+        <div className="whitespace-nowrap text-xl">
           {cart.total_after_discount.toFixed(2)} TND
         </div>
         {cart.total_after_discount < cart.total_before_discount && (
-          <del className="overflow-hidden whitespace-nowrap text-red-600">
+          <del className="whitespace-nowrap text-red-600">
             {cart.total_before_discount.toFixed(2)} TND
           </del>
         )}
