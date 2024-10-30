96644a6f8a3e87f2924c3dc696c8da65e93cf9d5
MontassarSN <snenmontassar@gmail.com>
2024-10-30 20:01:58 +0100

    Refactor addProduct page component and update form validation messages

--- src/app/(adminDashboard)/addProduct/page.tsx
+++ src/app/(adminDashboard)/addProduct/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { z } from "zod";
 import Image from "next/image";
@@ -8,34 +8,31 @@ import { uploadFile } from "@/api/uploadFile";
 import postData from "@/api/postData";
 import { SelectGeneric } from "@/app/ui/SelectGeneric";
 import { useToast } from "@/hooks/useToast";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import PictureUploader from "./ui/additional_pictures_uploader/picture_uploader";
 
-
 const schema = z.object({
-  title: z.string().min(1, "Title is required"),
-  subtitle: z.string().min(1, "Subtitle is required"),
-  description: z.string().min(1, "Description is required"),
-  price: z.number().positive("Price must be a positive number"),
+  title: z.string().min(1, "Le titre est requis"),
+  description: z.string().min(1, "La description est requise"),
+  price: z.number().positive("Le prix doit être un nombre positif"),
   discount: z.number().optional(),
-  stock: z.number().positive("Stock must be a positive number"),
-  wholesalePrice: z.number().positive("Wholesale price must be a positive number"),
-  category_id: z.union([
-    z.literal(1),
-    z.literal(2),
-    z.literal(3),
-  ], { message: "Invalid category" }),
+  stock: z.number().positive("Le stock doit être un nombre positif"),
+  wholesalePrice: z
+    .number()
+    .positive("Le prix de gros doit être un nombre positif"),
+  category_id: z.union([z.literal(1), z.literal(2), z.literal(3)], {
+    message: "Catégorie invalide",
+  }),
 });
-
 
 export default function Page() {
   const { toast } = useToast();
   const [preview, setPreview] = useState<string>("");
   const [images, setImages] = useState<File[]>([]);
   const queryClient = useQueryClient();
-
-  const Options: { label: string; value: string }[] = [
-    { label: "Concentration Games", value: "1" },
+  const formRef = useRef<HTMLFormElement>(null); // Create form reference
+  const Options = [
+    { label: "Jeux de concentration", value: "1" },
     { label: "Social", value: "2" },
     { label: "Construction", value: "3" },
   ];
@@ -50,44 +47,38 @@ export default function Page() {
 
   const AddArticleMutation = useMutation({
     mutationFn: async (formData: FormData) => {
-      const title = String(formData.get("title"));
-      const price = Number(formData.get("price"));
-      const discount = Number(formData.get("discount"));
-      const stock = Number(formData.get("stock"));
-      const description = String(formData.get("description"));
-      const subtitle = String(formData.get("subtitle"));
-      const wholesalePrice = Number(formData.get("wholesalePrice"));
-      const category_id = Number(formData.get("category_id"));
-      const filepicture = formData.get("filepicture") as File;
       const result = schema.safeParse({
-        title,
-        price,
-        discount,
-        stock,
-        description,
-        subtitle,
-        wholesalePrice,
-        category_id,
+        title: formData.get("title"),
+        price: Number(formData.get("price")),
+        discount: Number(formData.get("discount")),
+        stock: Number(formData.get("stock")),
+        description: formData.get("description"),
+        subtitle: formData.get("subtitle"),
+        wholesalePrice: Number(formData.get("wholesalePrice")),
+        category_id: Number(formData.get("category_id")),
       });
-
+  
       if (!result.success) {
-        throw new Error("Validation error");
+        const errorMessages = result.error.errors[0].message
+        throw new Error(errorMessages); 
       }
-
+  
+      // The rest of your mutation function remains unchanged
       let image_url = "";
-      if (filepicture.size > 0) {
+      const filepicture = formData.get("filepicture") as File;
+      if (filepicture && filepicture.size > 0) {
         image_url = await uploadFile({
           formData,
           name: "filepicture",
-          title:uuidv4(),
+          title: uuidv4(),
         });
       } else {
         throw new Error("Product Image is Required.");
       }
-
+  
       let extra_images_urls: string[] = [];
       if (images.length > 0) {
-        Promise.all(
+        await Promise.all(
           images.map(async (image) => {
             const formDataImage = new FormData();
             formDataImage.append("file", image);
@@ -100,43 +91,47 @@ export default function Page() {
           }),
         );
       }
-
-    const {error} =  await postData({
+  
+      const { error } = await postData({
         payload: [
           {
-            title,
-            price,
-            stock,
-            description,
-            subtitle,
-            discount,
+            title: String(formData.get("title")),
+            price: Number(formData.get("price")),
+            stock: Number(formData.get("stock")),
+            description: String(formData.get("description")),
+            subtitle: String(formData.get("subtitle")),
+            discount: Number(formData.get("discount")),
             discount_type: "fixed",
-            wholesale_price: wholesalePrice,
-            image_url: image_url || "",
+            wholesale_price: Number(formData.get("wholesalePrice")),
+            image_url,
             extra_images_urls,
-            category_id,
+            category_id: Number(formData.get("category_id")),
           },
         ],
         tableName: "products",
       });
-      if (error) {
-        throw new Error(error.message);
-      }
+      if (error) throw new Error(error.message);
     },
     onSuccess: () => {
       toast.success("Article added successfully!", "The product was created.");
       queryClient.invalidateQueries({ queryKey: ["products"] });
+  
+      formRef.current?.reset();
+      setPreview("");
+      setImages([]);
     },
-    onError: (error) => {
+    onError: (error: any) => {
       toast.error(
         "Error",
-        error.message || "An error occurred while adding the article.",
+        error.message || "An error occurred while adding the article."
       );
     },
   });
+  
 
   return (
     <form
+      ref={formRef} // Attach form reference
       action={AddArticleMutation.mutate}
       className="m-auto mt-20 flex w-full max-w-[50rem] flex-col justify-center gap-5 px-3 pb-4 sm:px-10"
     >
@@ -148,7 +143,7 @@ export default function Page() {
           width={15}
         />
         <div className="text-lg font-bold uppercase text-color5 sm:text-2xl">
-          Add A New Article
+          Ajouter un nouvel Produit
         </div>
         <Image
           src="/home/icons/flower_yellow.png"
@@ -158,16 +153,16 @@ export default function Page() {
         />
       </div>
 
-      <div className="m-auto flex w-full flex-col items-start gap-10 lg:flex-row mt-5" >
+      <div className="m-auto mt-5 flex w-full flex-col items-start gap-10 lg:flex-row">
         <div className="flex h-[15rem] w-full flex-col gap-5 lg:w-[30rem]">
           <div className="flex flex-col gap-3">
             <div className="text-sm font-semibold text-gray-900">
-              Product Title
+              Titre du produit
             </div>
             <input
               className="tx-lg w-full border-[1px] border-gray-200 px-4 py-2 text-gray-500 placeholder:text-sm placeholder:text-gray-300 focus:outline-none"
               type="text"
-              placeholder="Product Title..."
+              placeholder="Titre du produit..."
               name="title"
             />
           </div>
@@ -195,7 +190,7 @@ export default function Page() {
             src={preview || "/noArticlePic.png"}
             width={150}
             height={150}
-            alt="Article Picture"
+            alt="Image de l'article"
             className="m-auto"
           />
           <input
@@ -207,39 +202,40 @@ export default function Page() {
             onChange={handleImageChange}
           />
           <div className="mx-auto w-48 text-center text-xs text-gray-500">
-            Image size should be under 1MB and image ratio needs to be 1:1
+            La taille de l'image doit être inférieure à 1 Mo et le ratio d'image
+            doit être de 1:1
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-3 ">
+      <div className="flex flex-col gap-3">
         <div className="text-sm font-semibold text-gray-900">
-          Product Subtitle:
+          Sous-titre du produit :
         </div>
         <input
-          className="placeholder:text-sm placeholder:text-gray-300 border-gray-200 border-[1px] px-4 py-2 w-full tx-lg text-gray-500 focus:outline-none"
+          className="tx-lg w-full border-[1px] border-gray-200 px-4 py-2 text-gray-500 placeholder:text-sm placeholder:text-gray-300 focus:outline-none"
           type="text"
-          placeholder="Product Subtitle..."
+          placeholder="Sous-titre du produit..."
           name="subtitle"
         />
       </div>
 
       <div className="grid w-full grid-cols-1 items-center gap-3 sm:grid-cols-12">
         <div className="text-sm font-semibold text-gray-900 sm:col-span-2">
-          Original Price :
+          Prix d'origine :
         </div>
         <input
           className="tx-base w-full border-[1px] border-gray-200 px-4 py-2 text-gray-500 placeholder:text-sm placeholder:text-gray-300 focus:outline-none sm:col-span-4"
           type="number"
-          placeholder="Price..."
+          placeholder="Prix..."
           name="price"
         />
         <div className="text-sm font-semibold text-gray-900 sm:col-span-2">
-          Discount :
+          Remise :
         </div>
         <input
           className="tx-base w-full border-[1px] border-gray-200 px-4 py-2 text-gray-500 placeholder:text-sm placeholder:text-gray-300 focus:outline-none sm:col-span-4"
           type="number"
-          placeholder="Price After Discount..."
+          placeholder="remise..."
           name="discount"
         />
 
@@ -253,34 +249,35 @@ export default function Page() {
           name="stock"
         />
         <div className="text-sm font-semibold text-gray-900 sm:col-span-2">
-          Wholesale Price :
+          Prix de gros :
         </div>
         <input
           className="w-full border-[1px] border-gray-200 px-4 py-2 text-base text-gray-500 placeholder:text-sm placeholder:text-gray-300 focus:outline-none sm:col-span-4"
           type="number"
-          placeholder="Wholesale Price..."
+          placeholder="Prix de gros..."
           name="wholesalePrice"
         />
         <div className="text-sm font-semibold text-gray-900 sm:col-span-2">
-          Subtitle :
+          Catégories :
         </div>
         <SelectGeneric
           name="category_id"
           className="w-full placeholder:text-sm placeholder:text-gray-300 sm:col-span-4"
-          inputLabel="Select Subtitle..."
+          inputLabel="Sélectionnez la catégorie..."
           options={Options}
         />
       </div>
       <div className="text-sm font-semibold text-gray-900 sm:col-span-2">
-        Additional pictures:
+        Images supplémentaires :
       </div>
       <PictureUploader images={images} setImages={setImages} />
 
       <button
         className="mt-5 w-[10rem] rounded border border-slate-700 bg-slate-100 p-2 px-5 font-bold text-slate-700 duration-300 ease-in-out hover:!bg-slate-600 hover:!text-slate-200"
         type="submit"
+        disabled={AddArticleMutation.isPending}
       >
-        Submit
+        {AddArticleMutation.isPending ? "Chargement..." : "Soumettre"}
       </button>
     </form>
   );
